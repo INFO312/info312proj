bf4eb3b62d5722b7ec9363a44967f41ca8887568
Corey Sutton <sutco506@student.otago.ac.nz>
2015-09-15 19:36:40 +1200

    Gathers parameters from posted page

--- src/java/servlets/CreateDepartmentMemeberServelet.java
+++ src/java/servlets/CreateDepartmentMemeberServelet.java
@@ -6,13 +6,12 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 /**
  *
  * @author csutton
@@ -31,19 +30,35 @@ public class CreateDepartmentMemeberServelet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CreateDepartmentMemeberServelet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CreateDepartmentMemeberServelet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+       String fname = request.getParameter("fname");
+       String mname = request.getParameter("mname");
+       String lname = request.getParameter("lname");
+       String access = request.getParameter("access");
+       String gender = request.getParameter("gender");
+       
+       String email = request.getParameter("email");
+       String mphone = request.getParameter("mphone");
+       String hphone = request.getParameter("hphone");
+       String wphone = request.getParameter("wphone");
+       String dob = request.getParameter("dob");
+       
+       String address = request.getParameter("address");
+       String publications = request.getParameter("publications");
+       String otherInformation = request.getParameter("otherInformation");
+       
+       // Find the amount of expected qualifications and add to array
+       // while checking if any are empty
+       ArrayList<String> qualifications = new ArrayList<String>();
+       int qualificationCount = Integer.parseInt(request.getParameter("qualificationCounter"));
+       for(int i = 0; i < qualificationCount; i ++) {
+           String qualification = request.getParameter("qualification" + i);
+           if(qualification != "") {
+               qualifications.add(qualification);
+           }
+       }
+       
+               
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
