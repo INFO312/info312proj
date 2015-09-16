bbb2f8dfcb5487fe0d38562d58cfe752b1eb1a0f
Nathan <Nathan@10.249.149.153>
2015-09-17 11:21:21 +1200

    renamed this js file

--- web/scripts/root/addDepartmentMember.js
+++ web/scripts/root/addDepartmentMember.js
@@ -3,55 +3,67 @@ var amountOfDepartmentMembers = 1;
 
 function deleteDepartmentMember() {
     var departmentMemberNumber = $(this).val();
-    
+
     // Remove the delete button
     $(this).remove();
-    
+
     // Remove the departmentMember containter
     var departmentMemberToBeRemovedSelector = "select[name='departmentMember" + departmentMemberNumber + "']";
-    
+
     // Remoe the departmentMember container
     $(departmentMemberToBeRemovedSelector).parent().remove();
-    
-    amountOfDepartmentMembers --;
+
+    amountOfDepartmentMembers--;
     $("#hiddenDepartmentMemberCounter").val(amountOfDepartmentMembers);
 }
 
 function addAnotherDepartmentMember() {
-    
-    
-    if ($($(".departmentMemberSelector")[amountOfDepartmentMembers-1]).val() === "") {
+
+
+    if ($($(".departmentMemberSelector")[amountOfDepartmentMembers - 1]).val() === "") {
         alert("Please select a departmentMember before attempting to add another.");
     } else {
-         // Disable the previous departmentMember 
+        // Disable the previous departmentMember 
         $('.departmentMemberSelector').attr('disabled', true);
-        
+
         // Add the delete button
         $('<button type="button" name="deleteDepartmentMember" class="yellowButton" value=' + departmentMemberCounter + '>Delete</button><br>').insertBefore($(this));
         $('button[value="' + departmentMemberCounter + '"]').click(deleteDepartmentMember);
-        
+
         // Increment the counters
-        departmentMemberCounter ++;
-        amountOfDepartmentMembers ++;
-        
+        departmentMemberCounter++;
+        amountOfDepartmentMembers++;
+
         $("#hiddenDepartmentMemberCounter").val(amountOfDepartmentMembers);
-        
+
         // Add a new departmentMember selector
-        $('<div class="departmentMemberSelectorContainer"><label for="departmentMembers">DepartmentMembers</label><br><select name="qualification' + qualificationCounter + '" class="qualificationSelector"><option value =""></option><option value ="masterOfScience">Master of Science</option><option value ="masterOfBusiness">Master of Business</option><option value ="masterOfBusinessAdministration">Master of Business Administration</option><option value ="masterOfBusinessDataScience">Master of Business Data Science</option></select><br></div>').insertBefore(this);  
+        $('<div class="departmentMemberSelectorContainer">\n\
+<label for="departmentMembers">DepartmentMembers</label>\n\
+<br>\n\
+<select name="departmentMember' + departmentMemberCounter + '" class="departmentMemberSelector">\n\
+<option value =""></option>\n\
+<option value ="alecHolt">Alec Holt</option>\n\
+<option value ="brendonWoodford">Brendon Woodford</option>\n\
+<option value ="grantDick">Grant Dick</option>\n\
+<option value ="hankWolfe">Hank Wolfe</option>\n\
+<option value ="holgerRegenbrecht">Holger Regenbrecht</option>\n\
+</select>\n\
+<br>\n\
+</div>').insertBefore(this);
     }
-    
+
 }
 
 function setup() {
-    
+
     // On click add a new departmentMember
     $(".addAnotherDepartmentMemberButton").click(addAnotherDepartmentMember);
-    
+
     // Re-enable the selectors before submitting so the values are submitted
-    $('#createDepartmentMember').on('submit', function() {
+    $('#createDepartmentMember').on('submit', function () {
         $('.departmentMemberSelector').attr('disabled', false);
     });
-    
+
     // Set the value of the departmentMember counter
     $("#hiddenDepartmentMemberCounter").val(amountOfDepartmentMembers);
 }
