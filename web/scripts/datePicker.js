8da89242fd842330f4b8c8728d00c03a6b72e4f0
Corey Sutton <sutco506@student.otago.ac.nz>
2015-09-17 13:58:50 +1200

    Moved date picker to own file

---
+++ web/scripts/datePicker.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'dd-mm-yy',
+        defaultDate: '01-01-1990'
+    });
+});
