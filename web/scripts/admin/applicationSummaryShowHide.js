391225734c9675b4fcddfbdadc917fc1172be157
Corey Sutton <sutco506@student.otago.ac.nz>
2015-09-19 16:03:06 +1200

    Added script to toggle visible applications by date

---
+++ web/scripts/admin/applicationSummaryShowHide.js
@@ -0,0 +1,19 @@
+function setup() {
+
+    $(".applicationSummaryDate").click(function() {
+        
+        $(this).siblings(".applicationSummaryWrapper").toggle();
+        var currentArrow = $(this).children("span");
+        if ($(currentArrow).attr("class") === "downArrow") {
+            $(currentArrow).attr("class", "upArrow");
+            $(currentArrow).html('&#x25B2;');
+        } else {
+            $(currentArrow).attr("class", "downArrow");
+            $(currentArrow).html('&#x25BC;'); 
+        }
+       
+    });
+    
+}
+
+$(document).ready(setup);
